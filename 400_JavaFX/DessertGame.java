e61940227d31345739e56c8ee30e782277415d5c
jehrenstonemann <129352874+jehrenstonemann@users.noreply.github.com>
2023-03-29 23:53:23 -0500

    Add files via upload

---
+++ 400_JavaFX/DessertGame.java
@@ -0,0 +1,57 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import java.util.Random;
+
+public class DessertGame extends Application {
+
+    @Override
+    public void start(final Stage stage) {
+        // Step 3 & 4
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane, 640, 480);
+        stage.setTitle("Dessert in the Desert JavaFX Game");
+        
+        // Step 5
+        Label scoreLabel = new Label("Score: 0");
+        borderPane.setTop(scoreLabel);
+        BorderPane.setAlignment(scoreLabel, Pos.TOP_LEFT);
+
+        Button exitButton = new Button("Exit");
+        exitButton.setOnAction(event -> {
+            Platform.exit();
+        });
+        borderPane.setBottom(exitButton);
+        BorderPane.setAlignment(exitButton, Pos.BOTTOM_RIGHT);
+        
+        // Step 6
+        Pane pane = new Pane();
+        borderPane.setCenter(pane);
+        BorderPane.setAlignment(pane, Pos.CENTER);
+
+        // TODO: Step 7-10
+        // Step 7
+        Button dessert = new Button("Dessert");
+        Button desert1 = new Button("Desert");
+        Button desert2 = new Button("Desert");
+        Button desert3 = new Button("Desert");
+        Button desert4 = new Button("Desert");
+        Button desert5 = new Button("Desert");
+        Button desert6 = new Button("Desert");
+        Button desert7 = new Button("Desert");
+        
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+}
